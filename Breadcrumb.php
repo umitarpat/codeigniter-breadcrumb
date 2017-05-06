97402dd40d1fdf2dce2d2f4db60a347943f80c05
umitarpat <umitarpatkou@gmail.com>
2017-05-06 20:49:46 +0300

    Php File Upload

---
+++ Breadcrumb.php
@@ -0,0 +1,52 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Breadcrumb{
+	var $ci;
+	var $bread = '';
+	var $crumbs = array();
+	var $href_param;
+	var $seperator;
+	var $home_text;
+	var $home_link;
+
+	function Bc($seperator="",$href_param=NULL,$home_link='./',$home_text = "Anasayfa"){
+		$this->ci =& get_instance();
+		$this->href_param = $href_param;
+		$this->seperator = $seperator;	
+		$this->home_text = $home_text;
+		$this->home_link = $home_link;
+		$this->crumbs[] = array('crumb'=>$this->home_text,'link'=>$this->home_link);
+	}
+
+	function addCrumb($this_crumb,$this_link = NULL,$this_class = NULL){
+		$in_crumbs = false;
+		// first check that we haven't already got this link in the breadcrumb list.
+		foreach($this->crumbs as $crumb){
+			if($crumb['crumb'] == $this_crumb ){
+				$in_crumbs = true;
+			}
+			if($crumb['link'] == $this_link &&  $this_link != ''){
+				$in_crumbs = true;
+			}
+		}
+		if($in_crumbs == false){
+			$this->crumbs[] = array('crumb'=>$this_crumb,'link'=>$this_link);
+		}
+	}
+	// call this to return your breadcrumb html
+	
+	function makeBread(){
+		$sandwich = $this->crumbs;
+		$slices = array();
+		foreach($sandwich as $slice){
+			if (isset($slice['link']) && $slice['link'] != '') {
+				$slices[] = ' <i class="fa fa-angle-right"></i>  <a href="' . $slice['link'] . '" '.$this->href_param.'>' . $slice['crumb'] . '</a>';
+			} else {
+				$slices[] = ' <i class="fa fa-angle-right"></i>  <a>' . $slice['crumb'] . '</a>';
+			}	
+		}
+		$this->bread = join($this->seperator, $slices);
+		return $this->bread;
+	}
+}
